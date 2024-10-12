ffbb5200748ab1a55b567688ce1d9358fb8b97ed
adarsh0503 <adarshgaurao@gmail.com>
2024-10-12 23:41:31 +0530

    changed color to purple

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="bg-pink-500 p-9 text-center">
+    <header className="bg-purple-500 p-9 text-center">
       <h1 className="text-3xl font-bold text-white">My To-Do App</h1>
     </header>
   );
