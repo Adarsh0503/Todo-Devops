189dd7d2157489dff6e637d18dd392e921e0372e
adarsh0503 <adarshgaurao@gmail.com>
2024-10-13 10:54:51 +0530

    updated color

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="bg-pink-500 p-7 text-center">
+    <header className="bg-pink-600 p-7 text-center">
       <h1 className="text-4xl font-bold text-white">My To-Do App</h1>
     </header>
   );
