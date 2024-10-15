80c51912bdce20e4398ba1ddeb1353953dbfde13
adarsh0503 <adarshgaurao@gmail.com>
2024-10-15 21:01:49 +0530

    Updated some color

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,10 +2,17 @@ import React from 'react';
 
 const Header = () => {
   return (
+    <>
     <header className="bg-lime-600 p-8 text-center">
       <h1 className="text-4xl font-bold text-white">My To-Do App</h1>
     </header>
+
+    <footer className="bg-red-600 p-8 text-center">
+      <h1 className="text-4xl font-bold text-white">My To-Do App</h1>
+    </footer>
+
+    </>
   );
 };
 
-export default Header;c
+export default Header;
