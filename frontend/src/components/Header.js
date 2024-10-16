1f22de6ed4a5166b5b6137a72fd4e03d5d9afd55
adarsh0503 <adarshgaurao@gmail.com>
2024-10-16 09:52:40 +0530

    updated some colour

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <>
-    <header className="bg-blue-600 p-8 text-center">
+    <header className="bg-green-600 p-8 text-center">
       <h1 className="text-4xl font-bold text-white">My To-Do App</h1>
     </header>
 
